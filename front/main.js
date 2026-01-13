8b751e80a648c76cca6893dd6de6157e73c0a464
Dejan <zasluzujene@gmail.com>
2026-01-13 20:36:41 +0100

    UX changes

--- front/main.js
+++ front/main.js
@@ -17,45 +17,38 @@ function addTask(e) {
 	})
 		.then(res => res.json())
 		.then(displayTasks)
-		// .then(data => {     ovako stavimo umesto ovoga iznad .then(dispayTasks) kada debugujemo
+		// .then(data => {     ovako stavimo umesto ovoga iznad .then(displayTasks) kada debugujemo
 		// 	console.log('Success', data)
 		// })
 		.catch(err => console.error('Error adding task: ', err)
-	);
+		);
 }
 
 fetch('http://localhost:8888/index.php')  // Danilo je ovde imao http://localhost:8888/backtofront/back/index.php
 	.then(res => res.json())
-	.then(data => {
-		displayTasks(data);
-	})
+	.then(displayTasks)
 	.catch(err => console.error('Error fetching data: ', err)
-);
+	);
 
 
 function displayTasks(tasks) {
-		let html = '';
-		tasks.forEach(task => {
-			html += `
+	let html = '';
+	tasks.forEach(task => {
+		html += `
 			<div class="task" data-id="${task.id}">
 				<h3>${task.text}</h3>
 			</div>
 			`;
-		});
-		container.innerHTML = html;
-		let allTasks = document.querySelectorAll('.task');
-		allTasks.forEach((task, index) => {
-			task.addEventListener('click', deleteTask);
-			allTasks[index].addEventListener('dblclick', editTask) // cuvamo referencu na svaki task u this
-		});
+	});
+	container.innerHTML = html;
+	inputPlace.value = '';
+	let allTasks = document.querySelectorAll('.task');
+	allTasks.forEach((task, index) => {
+		task.addEventListener('click', deleteTask);
+		allTasks[index].addEventListener('dblclick', editTask) // cuvamo referencu na svaki task u this
+	});
 }
 
-function editTask(e) {
-	let h3 = this.querySelector('h3');
-	h3.setAttribute('contentEditable', 'true');
-	h3.focus();
-	h3.addEventListener('blur', saveEdit); // kada se izgubi fokus, poziva se funkcija
-}
 
 function saveEdit(e) {
 	let newText = this.innerText;
@@ -72,10 +65,17 @@ function saveEdit(e) {
 		.then(displayTasks)
 		.catch(err => console.error('Error editing task: ', err)
 		);
-	}
+}
+
+function editTask(e) {
+	let h3 = this.querySelector('h3');
+	h3.setAttribute('contentEditable', 'true');
+	h3.focus();
+	h3.addEventListener('blur', saveEdit); // kada se izgubi fokus, poziva se funkcija
+}
 
 function deleteTask(e) {
-	if(!e.altKey) return; // ako nije pritisnut alt, ne radi nista
+	if (!e.altKey) return; // ako nije pritisnut alt, ne radi nista
 	let id = this.getAttribute('data-id');
 	fetch(`http://localhost:8888/index.php?id=${id}`, { // Ovakva je praksa za DELETE. Danilo je ovde imao http://localhost:8888/backtofront/back/index.php
 		method: 'DELETE'
@@ -83,5 +83,5 @@ function deleteTask(e) {
 		.then(res => res.json())
 		.then(displayTasks)
 		.catch(err => console.error('Error deleting task: ', err)
-	);
+		);
 }
